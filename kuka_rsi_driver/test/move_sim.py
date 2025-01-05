4fccbdd4e5db8e6bbc8280cb02f5d2df221bb8f4
RobertWilbrandt <wilbrandt@fzi.de>
2025-01-05 13:50:42 +0100

    Implement execution tests (#11)
    * Add first execution test
    
    * Parametrize integration test prefixes and robot models
    
    * Make integration test more robust against startup timing
    
    * Move utility functions to new file
    
    * Move controller manager interfacing to custom class
    
    * Verify that controllers can get activated
    
    * Test jtc motion
    
    * Restructure activation tests and verify active controller
    
    * Add more logging to tests

---
+++ kuka_rsi_driver/test/move_sim.py
@@ -0,0 +1,251 @@
+# Copyright 2024 FZI Forschungszentrum Informatik
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the {copyright_holder} nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import sys
+import time
+import unittest
+import itertools
+import os
+import pytest
+import rclpy
+import rclpy.node
+import launch_testing
+
+from builtin_interfaces.msg import Duration
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from control_msgs.action import FollowJointTrajectory
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from launch_testing.actions import ReadyToTest
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+
+sys.path.append(os.path.dirname(__file__))
+from test_interface import ControllerManagerInterface, ActionInterface, subscribe_once
+
+
+@pytest.mark.launch_test
+@launch_testing.parametrize(
+    "prefix,robot_model",
+    itertools.product(
+        ["", "prefix_"],
+        [
+            ("kuka_kr5_support", "kr5_arc_macro.xacro", "kuka_kr5_arc"),
+            ("kuka_kr120_support", "kr120r2500pro_macro.xacro", "kuka_kr120r2500pro"),
+            ("kuka_kr210_support", "kr210r3100_macro.xacro", "kuka_kr210r3100"),
+        ],
+    ),
+)
+def generate_test_description(prefix, robot_model):
+    kuka_rsi_driver = FindPackageShare("kuka_rsi_driver")
+
+    test_description = LaunchDescription()
+
+    #
+    # Launch robot driver
+    #
+    robot_driver_launch = PathJoinSubstitution(
+        [kuka_rsi_driver, "launch", "test_bringup.launch.py"]
+    )
+    test_description.add_action(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(robot_driver_launch),
+            launch_arguments={
+                "description_package": robot_model[0],
+                "description_macro_file": robot_model[1],
+                "macro_name": robot_model[2],
+                "launch_rviz": "False",
+                "prefix": prefix,
+            }.items(),
+        )
+    )
+
+    #
+    # Start simulator
+    #
+    test_description.add_action(
+        Node(
+            package="kuka_rsi_driver",
+            executable="simulator",
+            arguments=["--host-ip", "127.0.0.1"],
+        )
+    )
+
+    test_description.add_action(ReadyToTest())
+    return test_description
+
+
+class MoveSimTest(unittest.TestCase):
+    MOTION_CONTROLLERS = ["joint_trajectory_controller", "forward_position_controller"]
+
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init()
+        cls.node = rclpy.node.Node("kuka_rsi_driver_test")
+        cls.controller_manager_interface = ControllerManagerInterface(cls.node)
+
+        MoveSimTest.test_cnt = 1
+        cls.log = cls.node.get_logger()
+
+        # Wait until spawners are done
+        cls._wait_for_spawners(cls)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.node.destroy_node()
+        rclpy.shutdown()
+
+    def setUp(self):
+        self.log = self.node.get_logger().get_child(f"{MoveSimTest.test_cnt}")
+        MoveSimTest.test_cnt += 1
+
+        print("\n")
+        self.log.info(f"Running test {self.id()}")
+
+    def test_activate_motion_controllers(self):
+        for controller in self.MOTION_CONTROLLERS:
+            with self.subTest(controller=controller):
+                self.log.info(f"Testing activation of motion controller '{controller}'")
+                self._activate_motion_controller(controller)
+
+                time.sleep(1)
+
+                self.log.info(f"Verifying state of '{controller}'")
+                controllers = self.controller_manager_interface.list_controllers()
+                active_controllers = [
+                    c.name for c in controllers if c.state == "active"
+                ]
+                self.assertIn(controller, active_controllers)
+                self.log.info(f"Successfully activated '{controller}'")
+
+    def test_move_jtc(self, prefix):
+        self._activate_motion_controller("joint_trajectory_controller")
+
+        # Create dummy trajectory
+        joint_names = [f"{prefix}joint_a{i+1}" for i in range(6)]
+        test_points = [
+            (Duration(sec=2, nanosec=0), [0.0 for j in joint_names]),
+            (Duration(sec=4, nanosec=0), [-1.0 for j in joint_names]),
+            (Duration(sec=8, nanosec=0), [1.0 for j in joint_names]),
+        ]
+        test_trajectory = JointTrajectory(
+            joint_names=joint_names,
+            points=[
+                JointTrajectoryPoint(positions=pos, time_from_start=time)
+                for (time, pos) in test_points
+            ],
+        )
+
+        # Send goal to jtc
+        self.log.info("Sending dummy trajectory to controller")
+        follow_joint_trajectory_client = ActionInterface(
+            "joint_trajectory_controller/follow_joint_trajectory",
+            FollowJointTrajectory,
+            self.node,
+            3,
+        )
+        result = follow_joint_trajectory_client.send_goal(
+            FollowJointTrajectory.Goal(trajectory=test_trajectory), timeout=9
+        )
+        self.assertEqual(result.error_code, FollowJointTrajectory.Result.SUCCESSFUL)
+
+        # Verify joint state
+        self.log.info("Verifying final position using joint_states")
+        joint_state = subscribe_once(
+            "/joint_states", JointState, self.node, 3, self.log
+        )
+
+        expected_state = test_points[-1][1]
+        for i, joint_name in enumerate(joint_names):
+            joint_i = joint_state.name.index(joint_name)
+            self.assertLess(
+                abs(expected_state[i] - joint_state.position[joint_i]), 0.01
+            )
+        self.log.info("Motion successfull")
+
+    def _wait_for_spawners(self):
+        expected_controllers_active = [
+            "joint_state_broadcaster",
+            "pose_broadcaster",
+            "status_broadcaster",
+            "joint_trajectory_controller",
+        ]
+        expected_controllers_inactive = ["forward_position_controller"]
+
+        # Wait until all controllers are loaded
+        self.log.info("Waiting for all controllers to be loaded")
+        for _ in range(10):
+            controllers = self.controller_manager_interface.list_controllers()
+
+            if len(controllers) == (
+                len(expected_controllers_active) + len(expected_controllers_inactive)
+            ):
+                break
+
+            self.log.info("Not all controllers are loaded yet - retrying")
+            time.sleep(1)
+
+        # Verify controller activations
+        self.log.info("Waiting for all spawners to be done")
+        for _ in range(5):
+            # Check controllers
+            controllers_active = [c.name for c in controllers if c.state == "active"]
+            controllers_inactive = [
+                c.name for c in controllers if c.state == "inactive"
+            ]
+
+            if (set(controllers_active) == set(expected_controllers_active)) and (
+                set(controllers_inactive) == set(expected_controllers_inactive)
+            ):
+                self.log.info("All controllers are in their expected state")
+                return
+
+            # Spawners are not yet done, so wait and retry
+            self.log.info("Not all controllers are fully spawned yet - retrying")
+            time.sleep(1)
+            controllers = self.controller_manager_interface.list_controllers()
+
+        # Previous calls did not show controller activation success => Fail
+        self.fail()
+
+    def _activate_motion_controller(self, controller):
+        self.log.info(
+            f"Activating motion controller '{controller}', deactivating {self.MOTION_CONTROLLERS} first"
+        )
+        self.controller_manager_interface.switch_controller(
+            deactivate=self.MOTION_CONTROLLERS, strict=False
+        )
+        self.controller_manager_interface.switch_controller(
+            activate=[controller], strict=True
+        )
+        self.log.info("  done")
---
+++ kuka_rsi_driver/test/test_interface.py
@@ -0,0 +1,233 @@
+# Copyright 2025 FZI Forschungszentrum Informatik
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the {copyright_holder} nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import time
+
+import rclpy
+from rclpy.action import ActionClient
+
+from controller_manager_msgs.srv import ListControllers, SwitchController
+from action_msgs.msg import GoalStatus
+
+
+class ControllerManagerInterface:
+    def __init__(self, node):
+        self._node = node
+        self._log = node.get_logger().get_child("controller_manager_interface")
+
+        self._list_controllers_client = wait_for_service(
+            "controller_manager/list_controllers", ListControllers, node, 10, self._log
+        )
+        self._switch_controllers_client = wait_for_service(
+            "controller_manager/switch_controller", SwitchController, node, 3, self._log
+        )
+
+    def list_controllers(self):
+        res = call_service(
+            self._list_controllers_client,
+            ListControllers.Request(),
+            self._node,
+            self._log,
+        )
+
+        if len(res.controller) > 0:
+            self._log.info("Controllers:")
+            for controller in res.controller:
+                self._log.info(
+                    f"  - {controller.name} ({controller.type}) - {controller.state}"
+                )
+        else:
+            self._log.info("Controllers: []")
+
+        return res.controller
+
+    def switch_controller(self, activate=[], deactivate=[], strict=True):
+        request = SwitchController.Request(
+            activate_controllers=activate,
+            deactivate_controllers=deactivate,
+            strictness=(
+                SwitchController.Request.STRICT
+                if strict
+                else SwitchController.Request.BEST_EFFORT
+            ),
+        )
+        res = call_service(
+            self._switch_controllers_client, request, self._node, self._log
+        )
+
+        if not res.ok:
+            error_msg = "Could not switch controllers"
+            self._log.error(error_msg)
+            raise RuntimeError(error_msg)
+
+        self._log.info(
+            f"Switched controllers ({'strict' if strict else 'best_effort'}):"
+        )
+        self._log.info(f"  - Activate: {activate}")
+        self._log.info(f"  - Deactivate: {deactivate}")
+
+
+class ActionInterface:
+    def __init__(self, action_name, action_type, node, timeout=10):
+        self._node = node
+        self._log = node.get_logger().get_child(action_name.split("/")[-1])
+
+        self._client = wait_for_action(
+            action_name, action_type, self._node, timeout, self._log
+        )
+
+    def send_goal(self, goal, timeout=10):
+        # Send goal
+        self._log.info(f"Sending goal to action server: {goal}")
+        goal_future = self._client.send_goal_async(goal)
+
+        self._log.info("  Waiting for goal acceptance")
+        rclpy.spin_until_future_complete(self._node, goal_future)
+
+        # Get goal handle
+        if goal_future.result() is None:
+            error_msg = (
+                f"Could not send goal to action server: {goal_future.exception()}"
+            )
+            self._log.error(f"  {error_msg}")
+            raise RuntimeError(error_msg)
+
+        goal_handle = goal_future.result()
+        if not goal_handle.accepted:
+            error_msg = "  Goal was not accepted"
+            self._log.error(f"  {error_msg}")
+            raise RuntimeError(error_msg)
+
+        uuid_str = "".join([f"{v:02x}" for v in goal_handle.goal_id.uuid])
+        self._log.info(f"  Goal {uuid_str} accepted")
+
+        # Wait for result
+        result_future = goal_handle.get_result_async()
+
+        self._log.info(f"  Waiting {timeout:.2f}s for result")
+        rclpy.spin_until_future_complete(self._node, result_future, timeout_sec=timeout)
+
+        # Check result
+        response = result_future.result()
+        if response is None:
+            error_msg = (
+                f"  Could not get result from action: {result_future.exception()}"
+            )
+        elif response.status != GoalStatus.STATUS_SUCCEEDED:
+            error_msg = (
+                f"  Goal did not succeed (status={response.status}): {response.result}"
+            )
+        else:
+            self._log.info(f"  Received result: {response.result}")
+            return response.result
+
+        self._log.error(f"  {error_msg}")
+        raise RuntimeError(error_msg)
+
+
+def wait_for_service(srv_name, srv_type, node, timeout=10, log=None):
+    if log is None:
+        log = node.get_logger()
+
+    client = node.create_client(srv_type, srv_name)
+
+    log.info(f"Waiting for service '{srv_name}' (timeout: {timeout:.2f}s)...")
+    if not client.wait_for_service(timeout):
+        error_msg = (
+            f"Could not reach service '{srv_name}' within timeout {timeout:.2f}s"
+        )
+        log.error(f"  {error_msg}")
+        raise RuntimeError(error_msg)
+
+    log.info(f"  Successfully waited for service '{srv_name}'")
+    return client
+
+
+def subscribe_once(topic, topic_type, node, timeout=10, log=None):
+    if log is None:
+        log = node.get_logger()
+
+    # Create subscriber
+    last_msg = None
+
+    def msg_cb(msg):
+        nonlocal last_msg
+        last_msg = msg
+
+    subscription = node.create_subscription(topic_type, topic, msg_cb, 1)
+
+    # Spin until receiving msg
+    log.info(
+        f"Waiting for message on topic {topic} ({topic_type.__name__}) for {timeout:.2f}s"
+    )
+    spin_start = time.time()
+    while (last_msg is None) and (time.time() < (spin_start + timeout)):
+        rclpy.spin_once(node, timeout_sec=0.1)
+
+    if last_msg is None:
+        error_msg = "Did not receive message within timeout"
+        log.error(f"  {error_msg}")
+        raise RuntimeError(error_msg)
+
+    log.info(f"  Received message: {last_msg}")
+
+    # Cleanup
+    subscription.destroy()
+    return last_msg
+
+
+def call_service(client, request, node, log=None):
+    if log is None:
+        log = node.get_logger()
+
+    log.info(f"Calling service '{client.srv_name}' with request '{request}'")
+    future = client.call_async(request)
+
+    rclpy.spin_until_future_complete(node, future)
+    if future.result() is None:
+        error_msg = f"Could not call service '{client.srv_name}': {future.exception()}"
+        log.error(f"  {error_msg}")
+        raise RuntimeError(error_msg)
+
+    log.info(f"  Received result: {future.result()}")
+    return future.result()
+
+
+def wait_for_action(action_name, action_type, node, timeout=10, log=None):
+    if log is None:
+        log = node.get_logger()
+
+    client = ActionClient(node, action_type, action_name)
+
+    log.info(f"Waiting for action '{action_name}' (timeout: {timeout:.2f}s)...")
+    if not client.wait_for_server(timeout):
+        error_msg = f"Could not reach action server '{action_name}' within timeout {timeout:.2f}s"
+        log.error(f"  {error_msg}")
+        raise RuntimeError(error_msg)
+
+    log.info(f"  Successfully waited for action server '{action_name}'")
+    return client
